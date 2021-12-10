342997998fa85ef3c931a0404a43bba6c1b65958
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-10 21:56:42 +0800

    ✨ Updated home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HackernewsService } from 'src/app/services/hackernews.service';
+import { FakernewsService } from 'src/app/services/fakernews.service';
+import type Story from 'src/app/models/hackernews/Item/Story';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.sass'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+    private readonly STORY_AMOUNT: number = 20;
+    stories: Array<Story> = [];
+    private subscriptionQueue: Array<Subscription> = [];
 
-  constructor() { }
+    constructor(private hnService: FakernewsService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.hnService.topstories().subscribe((storiesId: Array<number>) => {
+            for (let i = 0; i < this.STORY_AMOUNT; i++) {
+                const storyId = storiesId[i];
+                const subscriber = this.hnService
+                    .item<Story>(storyId)
+                    .subscribe((story: Story) => {
+                        this.stories.push(story);
+                    });
+                this.subscriptionQueue.push(subscriber);
+            }
+            console.log(this.stories);
+        });
+    }
 
+    ngOnDestroy(): void {
+        this.subscriptionQueue.forEach((subscription) => {
+            subscription.unsubscribe();
+        });
+    }
 }
