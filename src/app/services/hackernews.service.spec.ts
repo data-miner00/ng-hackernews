4b2e9c419090f4818f07526820ac0abece434a09
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-10 23:12:50 +0800

    ✅ Pass failing test

--- src/app/services/hackernews.service.spec.ts
+++ src/app/services/hackernews.service.spec.ts
@@ -1,16 +1,24 @@
+import {
+    HttpClient,
+    HttpClientModule,
+    HttpHandler,
+} from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
 import { HackernewsService } from './hackernews.service';
 
 describe('HackernewsService', () => {
-  let service: HackernewsService;
+    let service: HackernewsService;
+    let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HackernewsService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [HttpClient, HttpHandler],
+        });
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+        service = TestBed.inject(HackernewsService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
