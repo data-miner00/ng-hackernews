a877a0f644d95a01927c8810f0f648fec01be7bd
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-11 00:22:17 +0800

    ✅ Add and fix test

--- src/app/components/common/header/header.component.spec.ts
+++ src/app/components/common/header/header.component.spec.ts
@@ -3,23 +3,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HeaderComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should render the text correctly', () => {
+        const compiled = fixture.nativeElement as HTMLElement;
+
+        expect(compiled.textContent).toContain('Menu');
+        expect(compiled.textContent).toContain('Hackernews.');
+        expect(compiled.textContent).toContain('Login');
+    });
 });
