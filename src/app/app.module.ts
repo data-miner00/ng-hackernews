4554ee1494c6cefaeec635b59dd3b2fd2066eb16
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-10 21:56:23 +0800

    ✨ Added news item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HomeComponent } from './views/home/home.component';
 import { HeaderComponent } from './components/common/header/header.component';
 import { SidebarComponent } from './components/common/sidebar/sidebar.component';
 import { FooterComponent } from './components/common/footer/footer.component';
+import { NewsItemComponent } from './components/news-item/news-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, SidebarComponent, FooterComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, SidebarComponent, FooterComponent, NewsItemComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/news-item/news-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { elapsed, Time } from 'src/app/utils/date';
+
+@Component({
+    selector: 'app-news-item',
+    templateUrl: './news-item.component.html',
+    styleUrls: ['./news-item.component.sass'],
+})
+export class NewsItemComponent implements OnInit {
+    @Input() by?: string;
+    @Input() descendants?: number;
+    @Input() id?: number;
+    @Input() score?: number;
+    @Input() time?: number;
+    @Input() title?: string;
+    @Input() type?: string;
+    @Input() url?: string;
+    domain: string = '';
+    elapsed: number;
+
+    constructor() {}
+
+    ngOnInit(): void {
+        //@ts-ignore
+        const [_, domain] = /\/\/(.*?)\//.exec(this.url);
+        this.domain = domain;
+
+        this.elapsed = elapsed(this.time!, Time.Days);
+    }
+}
