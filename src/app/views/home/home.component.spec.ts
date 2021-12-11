9b3c52633b59d8c9cb3d114df9b5b9b57fab117b
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-11 19:29:09 +0800

    ✅ Added custom element schema

--- src/app/views/home/home.component.spec.ts
+++ src/app/views/home/home.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
@@ -8,9 +9,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
