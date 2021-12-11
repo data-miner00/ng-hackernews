d7811e2f4a3a25f4e3f63603bdd3dcd80e57de5d
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-12 01:56:49 +0800

    ✨ Add to route and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AskStoriesComponent } from './views/ask-stories/ask-stories.component';
 import { HomeComponent } from './views/home/home.component';
+import { JobStoriesComponent } from './views/job-stories/job-stories.component';
+import { ShowStoriesComponent } from './views/show-stories/show-stories.component';
+import { TopStoriesComponent } from './views/top-stories/top-stories.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'top-stories',
+        component: TopStoriesComponent,
+    },
+    {
+        path: 'ask-stories',
+        component: AskStoriesComponent,
+    },
+    {
+        path: 'show-stories',
+        component: ShowStoriesComponent,
+    },
+    {
+        path: 'job-stories',
+        component: JobStoriesComponent,
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,13 @@ import { HeaderComponent } from './components/common/header/header.component';
 import { SidebarComponent } from './components/common/sidebar/sidebar.component';
 import { FooterComponent } from './components/common/footer/footer.component';
 import { NewsItemComponent } from './components/news-item/news-item.component';
+import { TopStoriesComponent } from './views/top-stories/top-stories.component';
+import { AskStoriesComponent } from './views/ask-stories/ask-stories.component';
+import { ShowStoriesComponent } from './views/show-stories/show-stories.component';
+import { JobStoriesComponent } from './views/job-stories/job-stories.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, SidebarComponent, FooterComponent, NewsItemComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, SidebarComponent, FooterComponent, NewsItemComponent, TopStoriesComponent, AskStoriesComponent, ShowStoriesComponent, JobStoriesComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
