c21797815d5083a555390cb657c5fef8d9a22b5d
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-12 10:27:38 +0800

    ✨ Top stories page

--- src/app/views/top-stories/top-stories.component.ts
+++ src/app/views/top-stories/top-stories.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import type Story from 'src/app/models/hackernews/Item/Story';
+import { FakernewsService } from 'src/app/services/fakernews.service';
 
 @Component({
-  selector: 'app-top-stories',
-  templateUrl: './top-stories.component.html',
-  styleUrls: ['./top-stories.component.sass']
+    selector: 'app-top-stories',
+    templateUrl: './top-stories.component.html',
+    styleUrls: ['./top-stories.component.sass'],
 })
 export class TopStoriesComponent implements OnInit {
+    private STORY_AMOUNT: number = 20;
+    stories: Array<Story> = [];
+    private subscriptionQueue: Array<Subscription> = [];
 
-  constructor() { }
+    constructor(private hnService: FakernewsService) {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.hnService.topstories().subscribe((storiesId: Array<number>) => {
+            for (let i = 0; i < this.STORY_AMOUNT; i++) {
+                const storyId = storiesId[i];
+                const subscriber = this.hnService
+                    .item<Story>(storyId)
+                    .subscribe((story: Story) => {
+                        this.stories.push(story);
+                    });
+                this.subscriptionQueue.push(subscriber);
+            }
+        });
+    }
 }
