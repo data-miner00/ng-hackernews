cf38dac9c929d0de909dd6393168a365c5e9f8f5
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-13 19:50:00 +0800

    ✅ Added test

--- src/app/views/home/home.component.spec.ts
+++ src/app/views/home/home.component.spec.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home.component';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,10 +18,18 @@ describe('HomeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render section title accordingly', () => {
+    expect(compiled.textContent).toContain('Top Stories');
+    expect(compiled.textContent).toContain('Ask Stories');
+    expect(compiled.textContent).toContain('Show Stories');
+    expect(compiled.textContent).toContain('Job Stories');
   });
 });
