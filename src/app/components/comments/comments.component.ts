f5babd804608dc49e59396e2168916f3c98fd63c
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-17 21:50:37 +0800

    🚧 Added empty comments component

---
+++ src/app/components/comments/comments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.sass']
+})
+export class CommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
