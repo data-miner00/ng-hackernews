46f267824a61b7e2c287b46af1430c41fb4f4e09
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-21 22:10:38 +0800

    ✨ Simple recursive calling for comments

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import type Comment from 'src/app/models/hackernews/Item/Comment';
+import { FakernewsService } from 'src/app/services/fakernews.service';
+import { HackernewsService } from 'src/app/services/hackernews.service';
 
 @Component({
-  selector: 'app-comments',
-  templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.sass']
+    selector: 'app-comments',
+    templateUrl: './comments.component.html',
+    styleUrls: ['./comments.component.sass'],
 })
-export class CommentsComponent implements OnInit {
+export class CommentsComponent implements OnInit, OnDestroy {
+    @Input() commentId: number;
 
-  constructor() { }
+    comment: Comment;
+    commentSubscription: Subscription;
 
-  ngOnInit(): void {
-  }
+    constructor(private hnService: HackernewsService) {}
 
+    ngOnInit(): void {
+        this.commentSubscription = this.hnService
+            .item<Comment>(this.commentId)
+            .subscribe((comment) => {
+                this.comment = comment;
+            });
+    }
+
+    ngOnDestroy(): void {
+        if (this.commentSubscription !== null) {
+            this.commentSubscription.unsubscribe();
+        }
+    }
 }
