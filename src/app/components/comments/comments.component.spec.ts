c6c730773b7897ec3c79758b39839971f8b53ec6
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-25 23:22:02 +0800

    ✅ Fix tests

--- src/app/components/comments/comments.component.spec.ts
+++ src/app/components/comments/comments.component.spec.ts
@@ -1,21 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { CommentsComponent } from './comments.component';
+import { Type } from '@angular/core';
+import { HackernewsService } from 'src/app/services/hackernews.service';
+import { SafeHtmlPipe } from 'src/app/pipes/safe-html.pipe';
+import { of } from 'rxjs';
 
 describe('CommentsComponent', () => {
   let component: CommentsComponent;
   let fixture: ComponentFixture<CommentsComponent>;
+  let httpMock;
+  let spy: jasmine.Spy;
+  let mockHnService;
+  let serviceStub;
 
   beforeEach(async () => {
+    serviceStub = {
+      item: () =>
+        of({
+          id: 323456,
+          deleted: false,
+          type: 'comment',
+          by: 'duskblade',
+          time: 1639118165,
+          dead: false,
+          kids: [],
+          text: `<p>Hello world&#39;s</p>`,
+          parent: 123456,
+        }),
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ CommentsComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule],
+      declarations: [CommentsComponent, SafeHtmlPipe],
+      providers: [{ provide: HackernewsService, useValue: serviceStub }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CommentsComponent);
     component = fixture.componentInstance;
+
+    httpMock = fixture.debugElement.injector.get<HttpTestingController>(
+      HttpTestingController as Type<HttpTestingController>
+    );
     fixture.detectChanges();
   });
 
--- src/app/views/individual/individual.component.spec.ts
+++ src/app/views/individual/individual.component.spec.ts
@@ -1,21 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { DurationElapsedPipe } from 'src/app/pipes/duration-elapsed.pipe';
+import { FakernewsService } from 'src/app/services/fakernews.service';
+import type Story from 'src/app/models/hackernews/Item/Story';
 
 import { IndividualComponent } from './individual.component';
+import { HackernewsService } from 'src/app/services/hackernews.service';
 
 describe('IndividualComponent', () => {
   let component: IndividualComponent;
   let fixture: ComponentFixture<IndividualComponent>;
+  let hnService: FakernewsService;
+  let spy: jasmine.Spy;
+
+  const fakeActivatedRoute = {
+    params: of({ id: 123 }),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ IndividualComponent ]
-    })
-    .compileComponents();
+      declarations: [IndividualComponent, DurationElapsedPipe],
+      providers: [
+        { provide: ActivatedRoute, useValue: fakeActivatedRoute },
+        FakernewsService,
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IndividualComponent);
     component = fixture.componentInstance;
+
+    hnService = fixture.debugElement.injector.get(FakernewsService);
+
+    spy = spyOn(hnService, 'item').and.returnValue(
+      of<Story>({
+        id: 123456,
+        deleted: false,
+        type: 'story',
+        by: 'maxxx',
+        time: 1639118165,
+        dead: false,
+        kids: [],
+        descendants: 10,
+        score: 20,
+        title: 'Fizer is so good',
+        url: 'https://www.example.com/test/some.html',
+      })
+    );
+
     fixture.detectChanges();
   });
 
