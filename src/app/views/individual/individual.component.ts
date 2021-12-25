fdd017b273bf85a19c182304e939b88127ae7d3c
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-25 23:21:49 +0800

    ♻️ Refactor codes

--- src/app/views/individual/individual.component.ts
+++ src/app/views/individual/individual.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import type Story from 'src/app/models/hackernews/Item/Story';
 import { FakernewsService } from 'src/app/services/fakernews.service';
-import { elapsed, Time } from 'src/app/utils/date';
 
 @Component({
     selector: 'app-individual',
@@ -15,7 +14,6 @@ export class IndividualComponent implements OnInit, OnDestroy {
     posted: Date;
     routeSubscription: Subscription;
     storySubscription: Subscription;
-    elapsed: string;
 
     constructor(
         private route: ActivatedRoute,
@@ -34,7 +32,7 @@ export class IndividualComponent implements OnInit, OnDestroy {
     }
 
     ngOnDestroy(): void {
-        this.storySubscription.unsubscribe();
-        this.routeSubscription.unsubscribe();
+        if (this.storySubscription) this.storySubscription.unsubscribe();
+        if (this.routeSubscription) this.routeSubscription.unsubscribe();
     }
 }
