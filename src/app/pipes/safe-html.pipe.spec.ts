64f24f05c70a345a7f7bcda9ca346433656d2af3
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-26 12:35:41 +0800

    ✅ Pass safe html test

--- src/app/pipes/safe-html.pipe.spec.ts
+++ src/app/pipes/safe-html.pipe.spec.ts
@@ -1,8 +1,42 @@
-// import { SafeHtmlPipe } from './safe-html.pipe';
+import { SecurityContext } from '@angular/core';
+import {
+  DomSanitizer,
+  SafeHtml,
+  SafeResourceUrl,
+  SafeScript,
+  SafeStyle,
+  SafeUrl,
+  SafeValue,
+} from '@angular/platform-browser';
+import { SafeHtmlPipe } from './safe-html.pipe';
 
-// xdescribe('SafeHtmlPipe', () => {
-//   xit('create an instance', () => {
-//     const pipe = new SafeHtmlPipe();
-//     expect(pipe).toBeTruthy();
-//   });
-// });
+class DomSanitizerMock extends DomSanitizer {
+  sanitize(
+    context: SecurityContext,
+    value: string | SafeValue | null
+  ): string | null {
+    throw new Error('Method not implemented.');
+  }
+  bypassSecurityTrustHtml(value: string): SafeHtml {
+    throw new Error('Method not implemented.');
+  }
+  bypassSecurityTrustStyle(value: string): SafeStyle {
+    throw new Error('Method not implemented.');
+  }
+  bypassSecurityTrustScript(value: string): SafeScript {
+    throw new Error('Method not implemented.');
+  }
+  bypassSecurityTrustUrl(value: string): SafeUrl {
+    throw new Error('Method not implemented.');
+  }
+  bypassSecurityTrustResourceUrl(value: string): SafeResourceUrl {
+    throw new Error('Method not implemented.');
+  }
+}
+
+describe('SafeHtmlPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SafeHtmlPipe(new DomSanitizerMock());
+    expect(pipe).toBeTruthy();
+  });
+});
