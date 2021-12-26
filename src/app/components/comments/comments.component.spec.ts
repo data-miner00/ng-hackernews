13a8392f1b0271b15fbc95647d6de36ec6b67301
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-26 12:23:15 +0800

    ✅ Added comments tests

--- src/app/components/comments/comments.component.spec.ts
+++ src/app/components/comments/comments.component.spec.ts
@@ -5,10 +5,11 @@ import {
 } from '@angular/common/http/testing';
 
 import { CommentsComponent } from './comments.component';
-import { Type } from '@angular/core';
+import { DebugElement, Type } from '@angular/core';
 import { HackernewsService } from 'src/app/services/hackernews.service';
 import { SafeHtmlPipe } from 'src/app/pipes/safe-html.pipe';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('CommentsComponent', () => {
   let component: CommentsComponent;
@@ -44,6 +45,7 @@ describe('CommentsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CommentsComponent);
     component = fixture.componentInstance;
+    component.commentId = 323456;
 
     httpMock = fixture.debugElement.injector.get<HttpTestingController>(
       HttpTestingController as Type<HttpTestingController>
@@ -53,5 +55,35 @@ describe('CommentsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should bind story to property after being fetched', () => {
+    expect(component.comment.id).toBe(component.commentId);
+    expect(component.comment.by).toBe('duskblade');
+    expect(component.comment.kids?.length).toBe(0);
+    expect(component.comment.type).toBe('comment');
+  });
+
+  it('should populate `posted` based on calculations', () => {
+    const date = new Date(1639118165 * 1000);
+
+    expect(component.posted).toEqual(date);
+  });
+
+  it('should render to html accordingly based on data provided', () => {
+    const debugElement: DebugElement = fixture.debugElement;
+    const span = debugElement.query(By.css('.comment .author span'));
+    const author = debugElement.query(By.css('.comment .author'));
+    const comment = debugElement.query(By.css('.comment article'));
+
+    expect(span.nativeElement.textContent).toBe('duskblade');
+    expect(author.nativeElement.textContent).toContain('10 Dec, 2021');
+    expect(comment.nativeElement.textContent).toContain("Hello world's");
+  });
+
+  it('should not render any of the subcomments given there is none', () => {
+    const subcomments = fixture.debugElement.query(By.css('.subcomments'));
+
+    expect(subcomments.nativeElement.textContent).toBeFalsy();
   });
 });
