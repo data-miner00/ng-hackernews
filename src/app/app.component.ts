b6161d276e6c2cb39e7506970304bcb79b36438f
data-miner00 <data-miner00@users.noreply.github.com>
2022-02-09 19:16:01 +0800

    ✨ Scrolltop on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'ng-hackernews';
+    title = 'ng-hackernews';
+
+    onActivate(event: Event) {
+        window.scroll(0, 0);
+    }
 }
