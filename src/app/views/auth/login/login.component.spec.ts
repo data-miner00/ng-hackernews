bcb66791f04781186f143bc350c0c607b45b0594
data-miner00 <data-miner00@users.noreply.github.com>
2022-02-12 00:27:14 +0800

    ✅ Fix test

--- src/app/views/auth/login/login.component.spec.ts
+++ src/app/views/auth/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login.component';
 
@@ -8,9 +9,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [FormsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
