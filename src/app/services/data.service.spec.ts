6904934e0b5eebfee86d1d237d388974b17de766
data-miner00 <data-miner00@users.noreply.github.com>
2022-02-16 02:36:44 +0800

    ✅ Added event send test

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import { Subject } from 'rxjs';
 
 import { DataService } from './data.service';
 
 describe('DataService', () => {
   let service: DataService;
+  let subjectSpy: jasmine.SpyObj<Subject<undefined>>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(DataService);
+    subjectSpy = jasmine.createSpyObj('Subject', ['next']);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should send an event when triggerEvent is called', () => {
+    service.messageSource = subjectSpy;
+    service.triggerEvent();
+    expect(subjectSpy.next).toHaveBeenCalledWith(undefined);
   });
 });
