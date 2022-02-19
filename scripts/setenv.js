ee6e413913d7a7874c241d03c8f902957010b113
data-miner00 <data-miner00@users.noreply.github.com>
2022-02-19 11:13:59 +0800

    ✨ Adding checks for the variables

--- scripts/setenv.js
+++ scripts/setenv.js
@@ -7,6 +7,29 @@ const environment = argv.environment;
 
 const isProduction = environment === 'prod';
 
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const appId = process.env.FIREBASE_PROJECT_APP_ID;
+const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+const locationId = process.env.FIREBASE_LOCATION_ID;
+const apiKey = process.env.FIREBASE_API_KEY;
+const authDomain = process.env.FIREBASE_AUTH_DOMAIN;
+const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const measurementId = process.env.FIREBASE_MEASUREMENT_ID;
+
+if (
+    !projectId ||
+    !appId ||
+    !storageBucket ||
+    !locationId ||
+    !apiKey ||
+    !authDomain ||
+    !messagingSenderId ||
+    !measurementId
+) {
+    console.error('All the required environment variables were not provided!');
+    process.exit(-1);
+}
+
 const targetPath = isProduction
     ? `./src/environments/environment.prod.ts`
     : `./src/environments/environment.ts`;
@@ -14,14 +37,14 @@ const targetPath = isProduction
 const environmentFileContent = `
 export const environment = {
     firebase: {
-        projectId: '${process.env.FIREBASE_PROJECT_ID}',
-        appId: '${process.env.FIREBASE_PROJECT_APP_ID}',
-        storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
-        locationId: '${process.env.FIREBASE_LOCATION_ID}',
-        apiKey: '${process.env.FIREBASE_API_KEY}',
-        authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
-        messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
-        measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}',
+        projectId: '${projectId}',
+        appId: '${appId}',
+        storageBucket: '${storageBucket}',
+        locationId: '${locationId}',
+        apiKey: '${apiKey}',
+        authDomain: '${authDomain}',
+        messagingSenderId: '${messagingSenderId}',
+        measurementId: '${measurementId}',
     },
     production: ${isProduction},
 };
