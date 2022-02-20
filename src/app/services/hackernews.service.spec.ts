34db54367f017ca4c253c9c6ce6b278f28861d9d
data-miner00 <data-miner00@users.noreply.github.com>
2022-02-20 18:17:51 +0800

    ✅ Added tests for hackernews service

--- src/app/services/hackernews.service.spec.ts
+++ src/app/services/hackernews.service.spec.ts
@@ -1,24 +1,73 @@
 import {
-    HttpClient,
-    HttpClientModule,
-    HttpHandler,
-} from '@angular/common/http';
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { HackernewsService } from './hackernews.service';
 
+const baseUrl: string = 'https://hacker-news.firebaseio.com/v0';
+
 describe('HackernewsService', () => {
-    let service: HackernewsService;
-    let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let service: HackernewsService;
+  let httpMock: HttpTestingController;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [HttpClient, HttpHandler],
-        });
-        httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
-        service = TestBed.inject(HackernewsService);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HackernewsService],
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    service = TestBed.inject(HackernewsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  const httpEndpointTestScenarios = [
+    {
+      for: 'maxitem',
+      callHttpMethod: () => service.maxitem().subscribe(),
+      url: `${baseUrl}/maxitem.json`,
+    },
+    {
+      for: 'topstories',
+      callHttpMethod: () => service.topstories().subscribe(),
+      url: `${baseUrl}/topstories.json`,
+    },
+    {
+      for: 'askstories',
+      callHttpMethod: () => service.askstories().subscribe(),
+      url: `${baseUrl}/askstories.json`,
+    },
+    {
+      for: 'showstories',
+      callHttpMethod: () => service.showstories().subscribe(),
+      url: `${baseUrl}/showstories.json`,
+    },
+    {
+      for: 'jobstories',
+      callHttpMethod: () => service.jobstories().subscribe(),
+      url: `${baseUrl}/jobstories.json`,
+    },
+    {
+      for: 'updates',
+      callHttpMethod: () => service.updates().subscribe(),
+      url: `${baseUrl}/updates.json`,
+    },
+  ];
+
+  httpEndpointTestScenarios.forEach((scenario) => {
+    it(`should call ${scenario.url} when ${scenario.for} is invoked`, () => {
+      scenario.callHttpMethod();
+
+      const req = httpMock.expectOne(scenario.url);
+      expect(req.request.method).toBe('GET');
     });
+  });
 });
