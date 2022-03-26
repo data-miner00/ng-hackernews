9c1c2d3bac8483a370e62795e30636a79694d9f0
data-miner00 <data-miner00@users.noreply.github.com>
2022-03-27 00:06:01 +0800

    ✨ Add more functions to auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Auth, user, User } from '@angular/fire/auth';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import {
+    Auth,
+    createUserWithEmailAndPassword,
+    user,
+    User,
+    sendEmailVerification,
+    sendPasswordResetEmail,
+    updateProfile,
+} from '@angular/fire/auth';
+import {
+    doc,
+    DocumentData,
+    DocumentReference,
+    Firestore,
+    setDoc,
+} from '@angular/fire/firestore';
+import { signInWithEmailAndPassword, UserCredential } from 'firebase/auth';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 
@@ -10,7 +25,7 @@ import { take } from 'rxjs/operators';
 export class AuthService {
     user$: Observable<User | null>;
 
-    constructor(private auth: Auth) {
+    constructor(private auth: Auth, private firestore: Firestore) {
         this.user$ = user(auth);
     }
 
@@ -18,7 +33,45 @@ export class AuthService {
         return await this.user$.pipe(take(1)).toPromise();
     }
 
-    async emailLogin(email: string, password: string): Promise<any> {
+    async emailLogin(email: string, password: string): Promise<UserCredential> {
         return await signInWithEmailAndPassword(this.auth, email, password);
+    }
+
+    async emailSignup(email: string, password: string, displayName: string) {
+        const userCred: UserCredential = await createUserWithEmailAndPassword(
+            this.auth,
+            email,
+            password
+        );
+        this.sendEmailVerification();
+        this.createProfile(userCred.user, displayName);
+    }
+
+    async sendEmailVerification() {
+        const currentUser = this.auth.currentUser;
+        currentUser && (await sendEmailVerification(currentUser));
+    }
+
+    async forgotPassword(passwordResetEmail: string) {
+        await sendPasswordResetEmail(this.auth, passwordResetEmail);
+    }
+
+    async signOut() {
+        await this.auth.signOut();
+    }
+
+    async createProfile(user: User, displayName: string) {
+        updateProfile(user, {
+            displayName,
+        });
+
+        const userRef: DocumentReference<DocumentData> = doc(
+            this.firestore,
+            `users/${user.uid}`
+        );
+        setDoc(userRef, {
+            favourites: [],
+            readLater: [],
+        });
     }
 }
