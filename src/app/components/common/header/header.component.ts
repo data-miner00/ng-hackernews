6637932d703008e541e2cda4ff015631669a5bcd
data-miner00 <data-miner00@users.noreply.github.com>
2022-03-27 12:13:20 +0800

    🚧 Stage header

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 import { DataService } from 'src/app/services/data.service';
+import { User } from '@angular/fire/auth';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.sass'],
 })
-export class HeaderComponent {
-    public constructor(private dataService: DataService) {}
+export class HeaderComponent implements OnInit {
+    user: User | null;
+
+    public constructor(
+        private dataService: DataService,
+        public auth: AuthService
+    ) {}
+
+    async ngOnInit(): Promise<void> {
+        this.auth.user$.subscribe((user: User | null) => {
+            this.user = user;
+        });
+    }
 
     public toggleSidebar(): void {
         this.dataService.triggerEvent();
