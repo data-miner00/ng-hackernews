d32d09661555e01bd0e5e765016914d3c006c397
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-21 21:50:55 +0800

    ♻️ Replace figure with image comp

--- src/app/components/news-item-variant-v/news-item-variant-v.component.ts
+++ src/app/components/news-item-variant-v/news-item-variant-v.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NewsItemVariantVComponent implements OnInit {
     @Input() mode: string;
+    @Input() src: string;
+    @Input() description: string;
 
     constructor() {}
 
