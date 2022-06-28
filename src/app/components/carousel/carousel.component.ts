623c0724c2f8c4040d577eca1bf1042eec577a74
data-miner00 <data-miner00@users.noreply.github.com>
2022-06-28 21:13:57 +0800

    ✨ Working carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    OnInit,
+    ViewChild,
+} from '@angular/core';
 
 @Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.sass']
+    selector: 'app-carousel',
+    templateUrl: './carousel.component.html',
+    styleUrls: ['./carousel.component.sass'],
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements AfterViewInit {
+    @ViewChild('container') container: ElementRef<HTMLDivElement>;
 
-  constructor() { }
+    private count: number = 6;
+    private index: number = 0;
 
-  ngOnInit(): void {
-  }
+    constructor() {}
 
+    ngAfterViewInit(): void {
+        const imgs =
+            this.container.nativeElement.querySelectorAll('.animate-slideshow');
+        console.log(imgs);
+
+        setInterval(() => {
+            imgs[
+                this.index == 0 ? this.count - 1 : this.index - 1
+            ].classList.remove('active');
+
+            if (this.index == this.count - 1) this.index = 0;
+
+            imgs[this.index++].classList.add('active');
+        }, 7000);
+    }
 }
