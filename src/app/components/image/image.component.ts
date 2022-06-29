13f6549ebd376e6fbc08b78f7de8bcbf36728e8b
data-miner00 <data-miner00@users.noreply.github.com>
2022-06-29 20:14:17 +0800

    ♻️ Refactor image component

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -1,22 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-image',
     templateUrl: './image.component.html',
     styleUrls: ['./image.component.sass'],
 })
-export class ImageComponent implements OnInit {
+export class ImageComponent {
+    // The source of the image.
     @Input() src: string = '/assets/images/display3.webp';
 
-    @Input() inset: string = 'false';
+    // The property that indicates whether the caption will be fixed
+    // within the image frame.
+    @Input() inset: boolean;
 
-    // Caption
-
+    // The caption that describes the image.
     @Input() description: string =
         'A minuscule caption tries to be inconspicuous';
-    @Input() captionClass: string = '';
 
-    constructor() {}
-
-    ngOnInit(): void {}
+    // Optional class that will be applied by the caption.
+    // Available values: 'left', 'right', 'right-small'
+    @Input() captionClass: string;
 }
