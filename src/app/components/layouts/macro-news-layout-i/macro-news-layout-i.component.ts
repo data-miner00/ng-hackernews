d12c27ac52451f18a2186a237c3f0d252d5ad1bf
data-miner00 <data-miner00@users.noreply.github.com>
2022-07-27 23:44:24 +0800

    🤡 Use faker image

--- src/app/components/layouts/macro-news-layout-i/macro-news-layout-i.component.ts
+++ src/app/components/layouts/macro-news-layout-i/macro-news-layout-i.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FakerimgService } from 'src/app/services/fakerimg.service';
 
 @Component({
-  selector: 'app-macro-news-layout-i',
-  templateUrl: './macro-news-layout-i.component.html',
-  styleUrls: ['./macro-news-layout-i.component.sass']
+    selector: 'app-macro-news-layout-i',
+    templateUrl: './macro-news-layout-i.component.html',
+    styleUrls: ['./macro-news-layout-i.component.sass'],
 })
 export class MacroNewsLayoutIComponent implements OnInit {
+    constructor(private fakerimgService: FakerimgService) {}
 
-  constructor() { }
+    public imagePath: string;
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.imagePath = '/assets/images/' + this.fakerimgService.getImg();
+    }
 }
