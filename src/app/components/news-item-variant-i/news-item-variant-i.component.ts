76dcec72e4155c7f260b615f98f78e9962127677
data-miner00 <data-miner00@users.noreply.github.com>
2022-08-18 22:00:16 +0800

    🚑 Remove hardcoded values from template

--- src/app/components/news-item-variant-i/news-item-variant-i.component.ts
+++ src/app/components/news-item-variant-i/news-item-variant-i.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-news-item-variant-i',
-  templateUrl: './news-item-variant-i.component.html',
-  styleUrls: ['./news-item-variant-i.component.sass']
+    selector: 'app-news-item-variant-i',
+    templateUrl: './news-item-variant-i.component.html',
+    styleUrls: ['./news-item-variant-i.component.sass'],
 })
 export class NewsItemVariantIComponent implements OnInit {
+    @Input() id: number = 0;
+    @Input() title: string =
+        'Supreme Court Confirms Leak but Says Text Is Not Final';
+    @Input() description: string =
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate porro fugit nostrum autem repellendus ipsa dolorem debitis iusto, quo minima est eum tempore consequuntur repellat. Eius quam at esse deleniti.';
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
