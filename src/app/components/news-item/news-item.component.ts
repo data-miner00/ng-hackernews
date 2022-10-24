6de91b8a3876abc94e08d38c55448a7fccc0e062
data-miner00 <data-miner00@users.noreply.github.com>
2022-10-24 14:18:26 +0800

    ✨ Fresh look for news item component

--- src/app/components/news-item/news-item.component.ts
+++ src/app/components/news-item/news-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
     selector: 'app-news-item',
@@ -8,7 +10,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class NewsItemComponent implements OnInit {
     @Input() by?: string;
     @Input() descendants?: number;
-    @Input() id?: number;
+    @Input() id: number;
     @Input() score?: number;
     @Input() time?: number;
     @Input() title?: string;
@@ -18,7 +20,19 @@ export class NewsItemComponent implements OnInit {
     public domain: string;
     public posted: Date;
 
-    public ngOnInit(): void {
+    private userId: string;
+
+    public constructor(
+        public firestore: FirestoreService,
+        public auth: AuthService
+    ) {}
+
+    public get identiconUrl(): string {
+        const strId = String(this.id);
+        return `https://github.com/identicons/${strId.slice(0, 4)}.png`;
+    }
+
+    public async ngOnInit(): Promise<void> {
         if (this.time) {
             this.posted = new Date(this.time * 1000);
         }
@@ -35,5 +49,16 @@ export class NewsItemComponent implements OnInit {
         } else {
             this.domain = 'unknown.com';
         }
+
+        this.userId = (await this.auth.getUser())!.uid;
+    }
+
+    public addToFavourites() {
+        this.firestore.addToArrayAsync(
+            'users',
+            this.userId,
+            'favourites',
+            this.id
+        );
     }
 }
