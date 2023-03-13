b21ab2cf40dc7402ba6d5a878fa0d71fef564cf5
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-13 23:19:29 +0800

    ♻️ ✅ Use steps for sidebar tests

--- src/app/components/common/sidebar/sidebar.component.spec.ts
+++ src/app/components/common/sidebar/sidebar.component.spec.ts
@@ -1,75 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-
-import { SidebarComponent } from './sidebar.component';
+import { SidebarSteps } from './sidebar.component.steps';
 
 describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
-  let compiled: HTMLElement;
+  let steps: SidebarSteps;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SidebarComponent],
-    }).compileComponents();
-  });
+    steps = new SidebarSteps();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SidebarComponent);
-    component = fixture.componentInstance;
-    compiled = fixture.nativeElement;
-    fixture.detectChanges();
+    await steps.whenISetup();
+    steps.whenIDetectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should render content correctly', () => {
-    expect(compiled.textContent).toContain('Top Stories');
-    expect(compiled.textContent).toContain('Ask Stories');
-    expect(compiled.textContent).toContain('Job Stories');
-    expect(compiled.textContent).toContain('API by Y Combinator');
-    expect(compiled.textContent).toContain('2021 Shaun Chong');
+    steps.thenIExpectComponentToBeConstructed();
   });
 
   it('should be hidden initially', () => {
-    expect(compiled.querySelector('.overlay')).toHaveClass('hidden');
+    steps.thenIExpectSidebarToBeHidden();
   });
 
   it('should be shown and hidden when toggleSidebar is called accordingly', () => {
-    component.toggleSidebar();
-    expect(compiled.querySelector('.overlay')).not.toHaveClass('hidden');
-    component.toggleSidebar();
-    expect(compiled.querySelector('.overlay')).toHaveClass('hidden');
+    steps
+      .whenIToggleSidebar()
+      .thenIExpectSidebarToBeShown()
+      .whenIToggleSidebar()
+      .thenIExpectSidebarToBeHidden();
   });
 
   it('should be hidden when click event is fired on overlay', () => {
-    const overlay = compiled.querySelector('.overlay');
-
-    overlay?.classList.remove('hidden');
-    expect(overlay).not.toHaveClass('hidden');
-
-    fixture.debugElement
-      .query(By.css('.overlay'))
-      .triggerEventHandler('click', {});
-    expect(overlay).toHaveClass('hidden');
+    steps
+      .givenOverlayIsNotHidden()
+      .whenIClickOnOverlay()
+      .thenIExpectSidebarToBeHidden();
   });
 
   it('should not close when click inside the sidebar', () => {
-    const stopPropagationSpy = spyOn(
-      component,
-      'stopPropagation'
-    ).and.callThrough();
-    const overlay = compiled.querySelector('.overlay');
-    const sidebar = compiled.querySelector('nav') as HTMLElement;
-
-    overlay?.classList.remove('hidden');
-    expect(overlay).not.toHaveClass('hidden');
-
-    sidebar.click();
-
-    expect(stopPropagationSpy).toHaveBeenCalled();
-    expect(overlay).not.toHaveClass('hidden');
+    steps
+      .givenISpyOnStopPropagation()
+      .givenOverlayIsNotHidden()
+      .whenIClickOnSidebar()
+      .thenIExpectStopPropagationToBeCalled(1)
+      .thenIExpectSidebarToBeShown();
   });
 });
---
+++ src/app/components/common/sidebar/sidebar.component.steps.ts
@@ -0,0 +1,71 @@
+import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BaseSteps } from 'src/app/BaseSteps';
+import { SidebarComponent } from './sidebar.component';
+
+export class SidebarSteps extends BaseSteps<SidebarSteps, SidebarComponent> {
+    stopPropagationSpy: jasmine.Spy;
+
+    get overlay(): HTMLElement | null {
+        return this.compiled.querySelector('.overlay');
+    }
+
+    get sidebar(): HTMLElement | null {
+        return this.compiled.querySelector('nav');
+    }
+
+    get getClass(): SidebarSteps {
+        return this;
+    }
+
+    async whenISetup(): Promise<void> {
+        await TestBed.configureTestingModule({
+            declarations: [SidebarComponent],
+        }).compileComponents();
+
+        this.fixture = TestBed.createComponent(SidebarComponent);
+    }
+
+    givenOverlayIsNotHidden() {
+        this.overlay?.classList.remove('hidden');
+        return this;
+    }
+
+    givenISpyOnStopPropagation() {
+        this.stopPropagationSpy = spyOn(
+            this.component,
+            'stopPropagation'
+        ).and.callThrough();
+        return this;
+    }
+
+    whenIToggleSidebar() {
+        this.component.toggleSidebar();
+        return this;
+    }
+
+    whenIClickOnOverlay() {
+        this.baseEl.query(By.css('.overlay')).triggerEventHandler('click', {});
+        return this;
+    }
+
+    whenIClickOnSidebar() {
+        this.sidebar?.click();
+        return this;
+    }
+
+    thenIExpectSidebarToBeHidden() {
+        expect(this.overlay).toHaveClass('hidden');
+        return this;
+    }
+
+    thenIExpectSidebarToBeShown() {
+        expect(this.overlay).not.toHaveClass('hidden');
+        return this;
+    }
+
+    thenIExpectStopPropagationToBeCalled(times: number) {
+        expect(this.stopPropagationSpy).toHaveBeenCalled();
+        return this;
+    }
+}
