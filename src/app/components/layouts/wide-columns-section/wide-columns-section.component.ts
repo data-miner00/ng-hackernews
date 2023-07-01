810cae9e76e12057d54e9fe33c71ae55972188a5
data-miner00 <data-miner00@users.noreply.github.com>
2023-07-01 15:42:24 +0800

    ✨ Dynamic news items (second part)

--- src/app/components/layouts/wide-columns-section/wide-columns-section.component.ts
+++ src/app/components/layouts/wide-columns-section/wide-columns-section.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import type Story from 'src/app/models/hackernews/Item/Story';
 
 @Component({
-  selector: 'app-wide-columns-section',
-  templateUrl: './wide-columns-section.component.html',
-  styleUrls: ['./wide-columns-section.component.sass']
+    selector: 'app-wide-columns-section',
+    templateUrl: './wide-columns-section.component.html',
+    styleUrls: ['./wide-columns-section.component.sass'],
 })
 export class WideColumnsSectionComponent implements OnInit {
+    @Input() story: Story;
+    @Input() story2: Story;
+    @Input() story3: Story;
+    @Input() story4: Story;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
--- src/app/components/news-item-variant-vii/news-item-variant-vii.component.ts
+++ src/app/components/news-item-variant-vii/news-item-variant-vii.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-news-item-variant-vii',
-  templateUrl: './news-item-variant-vii.component.html',
-  styleUrls: ['./news-item-variant-vii.component.sass']
+    selector: 'app-news-item-variant-vii',
+    templateUrl: './news-item-variant-vii.component.html',
+    styleUrls: ['./news-item-variant-vii.component.sass'],
 })
 export class NewsItemVariantViiComponent implements OnInit {
+    @Input() id: number;
+    @Input() title: string;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
