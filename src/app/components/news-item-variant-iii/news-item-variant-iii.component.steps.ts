13a29bf2aa5545a40dbb2bc2fd45e2824fa75216
data-miner00 <data-miner00@users.noreply.github.com>
2023-07-03 20:30:09 +0800

    ✅ Added news item iii and iv tests

---
+++ src/app/components/news-item-variant-iii/news-item-variant-iii.component.steps.ts
@@ -0,0 +1,37 @@
+import { BaseSteps } from 'src/app/test-utils/BaseSteps';
+import { NewsItemVariantIiiComponent } from './news-item-variant-iii.component';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+export class NewsItemVariantIiiSteps extends BaseSteps<
+    NewsItemVariantIiiSteps,
+    NewsItemVariantIiiComponent
+> {
+    get getClass(): NewsItemVariantIiiSteps {
+        return this;
+    }
+
+    async givenISetupAsync(): Promise<void> {
+        await TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
+            declarations: [NewsItemVariantIiiComponent],
+        }).compileComponents();
+
+        this.fixture = TestBed.createComponent(NewsItemVariantIiiComponent);
+    }
+
+    givenIHaveRequiredProps(id: number, title: string, description: string) {
+        this.component.id = id;
+        this.component.title = title;
+        this.component.description = description;
+        this.component.avatar = '';
+
+        return this;
+    }
+
+    givenIHaveAvatarUrl(url: string) {
+        this.component.avatar = url;
+
+        return this;
+    }
+}
