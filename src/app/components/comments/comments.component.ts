d76aece17509bfa64b24ecd45bad73b83c570d76
data-miner00 <data-miner00@users.noreply.github.com>
2024-03-09 08:25:47 +0800

    ✨ Collapsable comment thread

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    Input,
+    OnDestroy,
+    OnInit,
+    ViewChild,
+} from '@angular/core';
 import { Subscription } from 'rxjs';
 import type Comment from 'src/app/models/hackernews/Item/Comment';
 import { FakernewsService } from 'src/app/services/fakernews.service';
@@ -11,12 +18,18 @@ import { HackernewsService } from 'src/app/services/hackernews.service';
 })
 export class CommentsComponent implements OnInit, OnDestroy {
     @Input() commentId: number;
+    @ViewChild('subcomments') subcomments: ElementRef<HTMLDivElement>;
 
     public comment?: Comment;
     public commentSubscription: Subscription;
+    public threadCollapsed = false;
     public posted: Date = new Date();
 
-    public constructor(private hnService: HackernewsService) {}
+    constructor(private readonly hnService: HackernewsService) {}
+
+    public get collapseButtonText(): string {
+        return this.threadCollapsed ? 'Expand' : 'Collapse';
+    }
 
     public ngOnInit(): void {
         this.commentSubscription = this.hnService
@@ -31,5 +44,10 @@ export class CommentsComponent implements OnInit, OnDestroy {
         if (this.commentSubscription !== null) {
             this.commentSubscription.unsubscribe();
         }
+    }
+
+    public toggleThreadCollapse(): void {
+        this.threadCollapsed = !this.threadCollapsed;
+        this.subcomments.nativeElement.classList.toggle('collapsed');
     }
 }
