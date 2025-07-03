a59fa9b359c3a73b7eacae7dcb0df3b177d8e406
Shaun Chong <data-miner00@users.noreply.github.com>
2025-07-03 22:04:57 +0800

    🔧 Add karma properties

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function (config) {
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
+        random: false,
       },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
@@ -43,5 +44,9 @@ module.exports = function (config) {
         flags: ['--no-sandbox'],
       },
     },
+    captureTimeout: 210000,
+    browserDisconnectTolerance: 3, 
+    browserDisconnectTimeout : 210000,
+    browserNoActivityTimeout : 210000,
   });
 };
