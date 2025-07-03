2cb0500562df991a1f0e399cc631a32ff03fce25
Shaun Chong <data-miner00@users.noreply.github.com>
2025-07-04 00:12:40 +0800

    ✨ Add favourite service

---
+++ src/app/services/favourite.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+import { LocalstoreService } from './localstore.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class FavouriteService {
+    private readonly FAVOURITES_KEY = 'favourites';
+
+    constructor(private localStore: LocalstoreService) {}
+
+    public getFavourites(): Array<string> {
+        const favourites = this.localStore.getItem(this.FAVOURITES_KEY);
+        return favourites ? JSON.parse(favourites) : [];
+    }
+
+    public addFavourite(storyId: string): void {
+        const favourites = this.getFavourites();
+        if (!favourites.includes(storyId)) {
+            favourites.push(storyId);
+            this.localStore.setItem(
+                this.FAVOURITES_KEY,
+                JSON.stringify(favourites)
+            );
+        }
+    }
+
+    public removeFavourite(storyId: string): void {
+        const favourites = this.getFavourites();
+        const index = favourites.indexOf(storyId);
+        if (index > -1) {
+            favourites.splice(index, 1);
+            this.localStore.setItem(
+                this.FAVOURITES_KEY,
+                JSON.stringify(favourites)
+            );
+        }
+    }
+}
