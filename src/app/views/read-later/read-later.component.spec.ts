25ff9d19f88b7b1a3b2a41295b1adf46494e1196
Shaun Chong <data-miner00@users.noreply.github.com>
2025-07-04 13:07:32 +0800

    🔥 Replace firebase in read later component

--- src/app/views/read-later/read-later.component.spec.ts
+++ src/app/views/read-later/read-later.component.spec.ts
@@ -1,42 +1,15 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import type User from 'src/app/models/hackernews/User';
-import { AuthService } from 'src/app/services/auth.service';
-import { FakernewsService } from 'src/app/services/fakernews.service';
-import { FirestoreService } from 'src/app/services/firestore.service';
-import { HackernewsService } from 'src/app/services/hackernews.service';
-
-import { ReadLaterComponent } from './read-later.component';
+import { ReadLaterComponentSteps } from './read-later.component.steps';
 
 describe('ReadLaterComponent', () => {
-  let component: ReadLaterComponent;
-  let fixture: ComponentFixture<ReadLaterComponent>;
-
-  const user: User = { id: 'user-id', created: 123, karma: 123 };
-
-  let mockAuthService = jasmine.createSpyObj(['getUser']);
-  mockAuthService.getUser.and.returnValue(Promise.resolve(user as any));
+  let steps: ReadLaterComponentSteps;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [ReadLaterComponent],
-      providers: [
-        { provide: FirestoreService, useValue: {} },
-        { provide: HackernewsService, useClass: FakernewsService },
-        { provide: AuthService, useValue: mockAuthService },
-      ],
-    }).compileComponents();
-  });
+    steps = new ReadLaterComponentSteps();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ReadLaterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    await steps.givenISetupAsync();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    steps.thenIExpectComponentToBeConstructed();
   });
 });
